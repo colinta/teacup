2bbad00cd8cc8c4a1771e3b6f0a464799caf5349
Vladimir Pouzanov <farcaller@gmail.com>
2012-05-16 18:36:00 +0300

    Separated layout behavior from style behavior

--- proposals/layout/layout_by_farcaller.rb
+++ proposals/layout/layout_by_farcaller.rb
@@ -1,5 +1,9 @@
 # The layout idea is based on beakr_stylesheet.rb, please see that file first.
 
+class InfoLabelStyle < Teacup::Style
+  textColor = :dark_gray
+end
+
 class MyCell < UITableViewCell
   include Teacup::Layout
   
@@ -18,12 +22,12 @@ class MyCell < UITableViewCell
     # The width must be specified, the height is set to be equal to the view
     # height. This implies autoresizing of flexible height and right margin.
     # Second argument is a view class, third is an alias (see later)
-    l.layout :left, UIImageView, :episode_image do |i|
+    l.layout_left, UIImageView, :episode_image do |i|
       i.width = 96
     end
     
     # Layout the remaining part
-    l.layout :right do
+    l.layout_right do
       # the right part is spilt into top / bottom layout
       
       # top part is a label with offsets. It still autolayouts as flexible width / bottom margin
@@ -32,43 +36,47 @@ class MyCell < UITableViewCell
       # *XXXX* TTTTTTTTT *
       # *XXXX*           *
       # ******************
-      #
-      l.layout :top, UILabel, :title do |i|
+      l.layout_top, UILabel, :title do |i|
         i.offsets left: 7, right: 7
-        i.font = UIFont.boldSystemFontOfSize(14)
+        
+        i.style do |s|
+          s.font = UIFont.boldSystemFontOfSize(14)
+        end
       end
       
-      l.layout :bottom do
-        # bottom part is complex again :)
-        
+      l.layout_bottom do
         # ******************
         # *XXXX* TTTTTTTTT *
         # *XXXX* LL        *
         # ******************
-        l.layout :left, UILabel, :length do |i|
+        l.layout_left, UILabel, :length do |i|
           i.width = 52
           i.offsets left: 7, top: 10
-          # override the default autoresize rules for :left
-          i.autoresize = flexible(:top_margin, :right_margin)
-          i.text_color = :dark_gray # use the common aliases from IB / UIColor
+          i.autoresize = flexible(:top_margin, :right_margin) # override the default autoresize rules for layout_left
+          
+          # you either define the style in a block
+          i.style do |s|
+            s.textColor = :dark_gray # use the common aliases from IB / UIColor
+          end
         end
         
         # ******************
         # *XXXX* TTTTTTTTT *
         # *XXXX* LL   DDDD *
         # ******************
-        l.layout :right, UILabel, :timestamp do |i|
+        l.layout_right, UILabel, :timestamp do |i|
           i.width = 78
           i.offsets right: 7, top: 10
-          # override the default autoresize rules for :right
-          i.autoresize = flexible(:top_margin, :left_margin)
-          i.text_color = :dark_gray
+          i.autoresize = flexible(:top_margin, :left_margin) # override the default autoresize rules for layout_right
+          
+          # or set the styling class
+          i.style = InfoLabelStyle
         end
       end
     end
     
-    # Note that parts are not created as actual views, they just simplify the
-    # relative size calculations
+    # Note that layout_* don't create additional views in the hierarchy, they just
+    # simplify frame calculations for relative elements.
   end
   
   def episode=(episode)
