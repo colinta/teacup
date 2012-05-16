87e9c2698e9ef76deaadce7860ae16a2725d179c
Chris Clarke <beakr@ninjanizr.com>
2012-05-16 15:53:34 -0700

    Added comment notice to contributors.rb

--- lib/teacup/contributors.rb
+++ lib/teacup/contributors.rb
@@ -1,6 +1,7 @@
 module Teacup
 
   # If you've made a contribution, add your name here in :' ... ' as a constant symbol.
+  # Please place names in aphabetical order.
   CONTRIBUTORS = [:'Chris Clarke', :'Colin Thomas-Arnold', :'Conrad Irwin', :'Roland Oth', :'Vladimir Pouzanov']
 
 end
