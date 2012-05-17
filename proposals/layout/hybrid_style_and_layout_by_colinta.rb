c485aaf62f40fa14f1ce54e8f3d5246dd2d11a4e
Colin Thomas-Arnold <colinta@gmail.com>
2012-05-17 15:48:26 -0600

    lots more changes and code

--- proposals/layout/hybrid_style_and_layout_by_colinta.rb
+++ proposals/layout/hybrid_style_and_layout_by_colinta.rb
@@ -11,17 +11,101 @@
 ###           * |======| mm/dd/YYYY   more info    *  <= smaller, gray captions
 ###           **************************************
 
-##| k, we're gonna need four properies when we instantiate a new
-##| ImageAndTextCell: image, title, date, and more_info
-##|
-##| that's my first big change from farcaller and conradirwin.
+  ##|
+  ##|  STYLE DECLARATIONS
+  ##|  based on conradirwin's nom/domnomery
+  ##|  I like the curlies, but they are optional
+  ##|
 
-class ImageAndTextCell < UITableCell
+
+# first, styles, broken out into a Stylesheet class
+# I'm using blocks here, a hash could also be used,
+# but the layout uses blocks, so point for consistency.
+# this would be in its own file.
+class MyStylesheet < Teacup::Style
+  s.style :shadow { |s|
+    shadow_color = :black.uicolor,
+    shadow_offset = 3,
+    shadow_radius = 1
+  }
+
+  style :icon { |s|
+    s.extends :shadow
+    s.margin = 2
+  }
+
+  s.style :title { |s|
+    s.font = :bold.uifont(14)  # see [sugarcube](https://github.com/colinta/sugarcube)
+  }
+
+  s.style :info { |s|
+    s.font = :small.uifont
+    # non UIColor objects will be converted by calling `uicolor`, so as long as
+    # sugarcube supports it, you're golden!
+    s.font_color = :gray
+  }
+
+  s.style :date { |s|
+    s.extends :info,
+    s.align = :left.uialignment,
+  }
+
+  s.style :more_info { |s|
+    s.extends :info,
+    s.align = :right.uialignment,
+    s.height = 20,
+  }
+
+  s.style :half_width { |s|
+    s.width = 50.percent
+  }
+
+  s.style :full_width {|s|
+    s.width = 100.percent
+  }
+
+  # I KNOW that accessory views are just an option on UITableCellView,
+  # but it makes a good example for why you would use a "box" to contain
+  # a few views, and then the button would be floated next to the box.
+  s.style :accessory_image {|s|
+    s.width = 10
+    s.height = 10
+    # the 'Percent' class will be special, so that we can do this kind of
+    # trickery:
+    s.margin_top = s.margin_bottom = 100.percent - 10
+  }
+end
+
+
+# k, when we creat an image & text cell, we're gonna need these four properies:
+# image, title, date, and more_info.  These get handed into some constructor,
+# could be a factory, or hash:
+#
+#     cell = ImageAndTextCell.alloc.init {
+#       title: 'My Title',
+#       image: 'the_image.png',
+#       date: '01/01/2012'
+#       more_info: 'neat!'
+#     }
+#
+#     cell = ImageAndTextCell.new {|v|
+#       v.title = 'My Title'
+#       v.image = 'the_image.png'
+#       v.date = '01/01/2012
+#       v.more_info = 'neat!'
+#     }
+#
+# the bulk of the work is done in `layout do...end`.  This code would normally
+# go in the ViewController, but that clutters the Controllers.  This encourages
+# building *views* in separate files, and instantiating those from the
+# controller (using code like above).
+class ImageAndTextCell < UITableCellView
   include Teacup::View
 
   # declaring these properies here makes them
   # available as arguments to the constructor
-  # and available as @property in the layout
+  # and available as @properties in the layout.
+  #
   # plus, the `synthesize` keyword is a Cocoa
   # staple, so Cocoa devs will immediately know
   # what is up.  of coure, `attr` is more rubyesque...
@@ -33,64 +117,8 @@ class ImageAndTextCell < UITableCell
   synthesize :more_info, :required => false  # or :default => 'something'
 
   ##|
-  ##|  STYLE DECLARATIONS
-  ##|  based on conradirwin's nom/domnomery
-  ##|  I like the curlies, but they are optional
-  ##|
-  styles do |s|
-    s.style :icon, {
-      margin: 2
-    }
-
-    s.style :shadow, {
-      shadow_color: :black.uicolor,
-      shadow_offset: 3,
-      shadow_radius: 1
-    }
-
-    s.style :title, {
-      font: :bold.uifont(14)  # see sugarcube
-    }
-
-    s.style :info, {
-      font: :system.uifont(8)
-    }
-
-    s.style :date, {
-      like: :info,
-      align: :left.uialignment,
-    }
-
-    s.style :more_info, {
-      like: :info,
-      align: :right.uialignment,
-      height: 20,
-    }
-
-    s.style :half_width, {
-      width: 50.percent
-    }
-
-    ##|  another way to do them would be to use a block:
-    s.style :full_width {|s|
-      s.width = 100.percent
-    }
-    s.style :accessory_image {|s|
-      s.width = 10
-      s.height = 10
-      # the 'Percent' class will be special, so that we can do this kind of
-      # trickery:
-      s.margin_top = s.margin_bottom = 100.percent - 10
-    }
-    ##|  I think I like the block syntax a little more... consistent
-    ##|  with the layout declaration below
-  end
-
-
-  ##|
   ##|  LAYOUT DECLARATION
-  ##|  called in init to construct our views?
-  ##|  called manually, returning a new view?
+  ##|  called in init, after initialization, to construct our views
   ##|
   layout do |layout|
     layout.height = 30
@@ -98,7 +126,7 @@ class ImageAndTextCell < UITableCell
     if @image
       # ImageView is inherited from Teacup::View
       # uses shadow and icon styles
-      layout << ImageView do |v|
+      image_view = ImageView do |v|
         # long hand, short hand (so you can use styles from other classes)
         v.styles = self.styles[shadow:], :icon
         v.image = @image.uiimage  # String to UIImage!?  see sugarcube
@@ -106,15 +134,50 @@ class ImageAndTextCell < UITableCell
         # image will be scaled to fit, so a 100x40 image will become 50x20
         v.width = 50
       end
+
+      # HMMMM - binding changes!?
+#     self.on :change_image do
+#     self.on :change, :image do
+#     self.on change(:image) do
+      self.on :change, :image do |image|
+        image_view.image = @image  # again, since this isn't a UIImage instance, `.uiimage` will get called on it.
+        self.setNeedsDisplay
+      end
+      layout << image_view
     end
 
-    layout << Label do |v|
+    layout << title_view = Label do |v|
       v.width = 100.percent  # if the icon is missing, this will be the entire width of the cell
       v.height = 30
       v.text = @title
       v.alignment = :center.uialignment  # returns UITextAlignmentCenter
     end
+    # BINDING title change
+    self.on :change, :title do |title|
+      title_view.text = title
+    end
 
+    # that binding thing is cool, huh?  i think so anyway...
+    # i can imagine instances where you need to monitor when *multiple*
+    # properties change
+    self.on :change, :title, :date do |title, date|
+      title_view.text = title + date
+    end
+    # it would be great if only one change event got fired when setting
+    #
+    #     image_view.title = 'foo'
+    #     image_view.date = 'foo'
+    #
+    # but you would have to wrap these somehow
+    #
+    #     image_view.begin_edit
+    #     image_view.title = 'foo'
+    #     image_view.date = 'foo'
+    #     image_view.end_edit  # fires
+    #
+    # which is fine, just something to think about.
+
+    # moving on - let's try and add a date and/or more_info
     if @date or @more_info
       # views are pretty expensive, so I'd prefer if this object "acted" like
       # a view, but actually just handled margins, floating, and such.
@@ -142,8 +205,9 @@ class ImageAndTextCell < UITableCell
       end
     end
 
-    self << ImageView(:accessory_image) do |v|
-      v.image = 'arrow.png'.uiimage
+    layout << ImageView(:accessory_image) do |v|
+      v.float = :right      # again, sugarcube will resolve these into the "correct type"
+      v.image = 'arrow.png' # for us.  happy day for duck typing!  :-)
     end
   end
 
