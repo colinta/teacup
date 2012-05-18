db50282f9b82e01447a59ba8aa77ea50ef2d61a1
Chris Clarke <beakr@ninjanizr.com>
2012-05-17 22:37:15 -0230

    Fixed gemspec authors issue.

--- lib/teacup/contributors.rb
+++ lib/teacup/contributors.rb
@@ -2,6 +2,6 @@ module Teacup
 
   # If you've made a contribution, add your name here in :' ... ' as a constant symbol.
   # Please place names in aphabetical order.
-  CONTRIBUTORS = [:'Chris Clarke', :'Colin Thomas-Arnold', :'Conrad Irwin', :'Roland Oth', :'Vladimir Pouzanov']
+  CONTRIBUTORS = ['Chris Clarke', 'Colin Thomas-Arnold', 'Conrad Irwin', 'Roland Oth', 'Vladimir Pouzanov']
 
 end
