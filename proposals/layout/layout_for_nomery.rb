75e866cd01dd92cfe266cb20f24700e4ae19442a
Conrad Irwin <conrad.irwin@gmail.com>
2012-05-20 23:07:29 -0700

    Update nomery proposal to be less magic

--- proposals/layout/layout_for_nomery.rb
+++ proposals/layout/layout_for_nomery.rb
@@ -1,86 +1,144 @@
+# A proposal that tries to take the best of the previous nomery proposal,
+# without the objectionable magic; to end up somewhere close to colinta's
+# teacup proposal.
+#
+# Description of APIs:
+#
+#   UIViewController.layout(style_name, &block) can be used to declare the
+#   layout of this controller. The style_name is added to the 'view', and
+#   the block is instance_exec'd in viewDidLoad (after which layoutDidLoad
+#   is called).
+#
+#   Teacup::Layout#layout(class_or_instance, style_name, style_properties, &block)
+#   (aka. UIViewController#layout,  UIView#layout) can be used to fix up the
+#   layout.
+#
+#   It apples the style_name and style_properties to the instance (creating a new
+#   instance if a class was passed), and then adds this instance to the current view.
+#
+#   If the block is passed it is called, and any calls to "layout" within the block
+#   will add further subviews to the newly styled view.
+#
+#   UIView#style_name, UIView#style_sheet
+#   These are attr_accessor, and when you change them the view's style is recomputed.
+#
+#   UIViewController#style_sheet
+#   The user must implement this function to determine the current stylesheet.
+#
+# Main differences from colinta's version:
+#
+# 1. The nesting is done automatically by block inclusion.
+#   e.g. to add a new subview to 'foo':
+#
+#      layout(foo) do
+#        layout(UIView.new, :subview)
+#      end
+#
+#   this is because you otherwise end up with lots of view << foo = bar,
+#   which is very ugly
+#
+# 2. Instead of using upper-case-methods as constructors, use a layout()
+#    function that accepts a class. This works better with people's custom
+#    UIView sub-classes and feels less magic.
+#
+# 3. Instead of using 'synthesise' to push variables into the layout; just
+#    evaluate the layout in the context of the controller. Then people can
+#    use attr_accessor as normal.
+#
+# Main difference from the original:
+#
+# 1. Doesn't automatically create view objects or assign them to controller
+#    attributes; you have to be explicit about it. Feels much less magic.
+#
+# 2. Stores pointers from the UIView objects back to the style_sheet so that
+#    changing style_name on a view causes a view recalculation.
+#
+# 3. Evals the layout in the controller, so you can set properties like
+#    delegate there to real objects.
+#
+# 4. Makes 'layout' available for UIViews as well for homogenity.
+#
 class CommuneViewController < UIViewController
 
-  # A proposal for 'layout' that goes alongside my proposal for
-  # stylsheets.
-  #
-  # There's a badly implemented version of it working on the Commune app,
-  # https://github.com/ConradIrwin/Commune/tree/master/app/controllers/commune_view_controller.rb
-  # 
-  # The layout does two things, 1. constructs the UIViews in the necessary
-  # order on viewDidLoad, and then calls layoutDidLoad. and 2. creates methods
-  # to access all the created views.
-  #
-  # For convenience, it can also create an inline stylesheet (though actually
-  # this behaviour may be rubbsih given how complicated the implementation of
-  # style_sheet then becomes.
-  #
-  # TODO: This doesn't yet have facility to create subviews dynamically.
-  #
-  #
-  layout :commune_view do
-    subview :how_much
-    subview :what_for
-    subview :who_paid
-    subview :who_participated
-    subview :amount
-    subview :event
-    subview :commune_it
-    subview :paid
-    subview :participated
-    # Look, you can nest stuff!
-    subview :shiny_thing do
-      # And you can put inline styles on stuff!
-      subview :shadow_view, {
-        top: 100,
-        width: 100,
-        height: 100,
-        left: 550,
-        backgroundColor: UIColor.blackColor
-      } do
-        subview :rounded_view, {
-          class: UILabel,
-          cornerRadius: 40,
-          top: 10,
-          left: 10,
-          width: 80,
-          height: 80,
-          backgroundColor: UIColor.blueColor
-        }
-      end
-    end
-  end
+  attr_accessor :amount, :event, :commune_it, :paid, :participated
 
-  # Easy to switch style-sheets when the time comes.
-  def willAnimateRotationToInterfaceOrientation(io, duration: duration)
-    layout.style_sheet = style_sheet
-  end
+  layout(:commune_view) do |view|
+    layout(UILabel, :how_much)
+    layout(UILabel, :what_for)
+    layout(UILabel, :who_paid)
+    layout(UILabel, :who_participated)
 
-  def viewDidLoad
-     # Take the declaration above and make lots of subviews!
-     # we might want to put this somewhere central...
-     self.layout = Teacup::Layout.new(view, style_sheet, self.class.layout_definition)
+    self.amount = layout(UITextField, :amount, delegate: self)
+    self.event = layout(UITextField, :event, delegate: self)
 
-    # Woah, did you notice, I never assigned to these anywhere!!!
-    amount.delegate = self
-    event.delegate = self
+    self.commune_it = layout(UIButton.buttonWithType(UIButtonTypeCustom), :commune_it)
     commune_it.addTarget(self, action: :click, forControlEvents:UIControlEventTouchUpInside)
 
-    paid.people = Person::ALL
-    participated.people = Person::ALL
+    self.paid = layout(PersonList::SelectOne, :paid, people: Person::ALL)
+    self.participated = layout(PersonList::SelectMany, :participated, people: Person::ALL)
+
+    layout(UIView,
+              top: 100,
+              width: 100,
+              height: 100,
+              left: 550,
+              backgroundColor: UIColor.blackColor
+            ) do |shiny_thing|
+              layout(UIView,
+                cornerRadius: 30,
+                top: 10,
+                left: 10,
+                width: 80,
+                height: 80,
+                backgroundColor: UIColor.blueColor
+              )
+            end
   end
-  
-   # Eww, icky, yucky, eeww..
-   # Maybe we should some how move the 'class's style_sheet more internally...
+
   def style_sheet
-    super_sheet = self.class.style_sheet
-    Teacup::StyleSheet.new(:Temp) do
-      if UIDevice.currentDevice.orientation == UIDeviceOrientationLandscapeLeft ||
-         UIDevice.currentDevice.orientation == UIDeviceOrientationLandscapeRight
-        include Teacup::StyleSheet::IPad
-      else
-        include Teacup::StyleSheet::IPadVertical
-      end
-      include super_sheet
+    if [UIDeviceOrientationLandscapeLeft,
+        UIDeviceOrientationLandscapeRight].include?(UIDevice.currentDevice.orientation)
+      Teacup::StyleSheet::IPad
+    else
+      Teacup::StyleSheet::IPadVertical
     end
+  end
+
+  def textFieldShouldReturn(textField)
+    if textField == amount
+      event.becomeFirstResponder
+    else
+      event.resignFirstResponder
+    end
+  end
+
+  def click
+    form = Form.new(amount.text, @paid.selection, @participated.selection, event.text)
+    return alert(form.validation_errors.join(", ")) unless form.valid?
+
+    response = form.submit!
+
+    if response =~ /Your response has been recorded./
+      alert('w00t!, response recorded!');
+    else
+      $stderr.puts response
+      alert('ZOMG, it didn"t work :(');
+    end
+  end
+
+  def shouldAutorotateToInterfaceOrientation(io)
+    true
+  end
+
+  def willAnimateRotationToInterfaceOrientation(io, duration: duration)
+    restyle!
+  end
+
+  def alert(msg)
+    alert = UIAlertView.new
+    alert.title = "Commune!"
+    alert.message = msg
+    alert.addButtonWithTitle("OK")
+    alert.show
   end
 end
