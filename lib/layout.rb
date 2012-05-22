e778f8f947e48f6b2df3254f61a218274ea41be6
Conrad Irwin <conrad.irwin@gmail.com>
2012-05-22 00:20:23 -0700

    Document and de-underscore method names.

--- lib/layout.rb
+++ lib/layout.rb
@@ -6,16 +6,83 @@ module Teacup
   # should be available when you need them.
   #
   # In order to use layout() in a UIViewController most effectively you will want
-  # to create a stylesheet method
+  # to define a stylesheet method that returns a stylesheet.
+  #
+  # @example
+  #   class MyViewController < UIViewController
+  #     interface(:my_view) do
+  #       layout UIImage, :logo
+  #     end
+  #  
+  #     def stylesheet
+  #       Teacup::Stylesheet::Logo
+  #     end
+  #   end
+  #
   module Layout
-    def style_sheet
-      nil
-    end
 
-    def restyle!
-      top_level_view.style_sheet = style_sheet
-    end
-
+    # Define or alter the layout for a view.
+    #
+    # @param class_or_instance  The first parameter is the view that you want to
+    #                           layout. Though for convenience you can also pass
+    #                           a UIView subclass and a new view will be created
+    #                           for you.
+    #
+    #                           If a new view is created, or the passed view is
+    #                           not yet present in the view heirarchy it will be
+    #                           added at the current level. (see &block)
+    #
+    # @param name               The second parameter is optional, and is the
+    #                           stylename to apply to the element. When using
+    #                           stylesheets any properties defined in the
+    #                           current stylesheet (see {stylesheet}) for this
+    #                           element will be immediately applied.
+    #
+    # @param properties         The third parameter is optional, and is a Hash
+    #                           of properties to apply to the view directly.
+    #
+    # @param &block             If a block is passed, it is evaluated such that
+    #                           any calls to {layout} that occur within that
+    #                           block cause created subviews to be added to *this*
+    #                           view instead of to the top-level view.
+    #
+    # For example, when creating a layout in the interface block of a
+    # constructor, you might want to add several labels:
+    # 
+    # @example
+    #   interface(:my_view_controller) do
+    #     layout(UILabel, :left_label, text: "One")
+    #     layout(UILabel, :left_label, text: "Two")
+    #   end
+    #
+    # You might also need to create nested structures in order to get perfect
+    # view effects:
+    # 
+    # @example
+    #   interface(:my_view_controller) do
+    #     layout(UIView, :shiny_thing) {
+    #       layout(UIView, :glow) {
+    #         layout(UIView, :star)
+    #       }
+    #       layout(UIView, :moon)
+    #     }
+    #   end
+    #
+    # Use of the layout function is not restricted to defining interfaces
+    # though, it can also be used to alter existing views.
+    #
+    # For example, to add a new image to a carousel:
+    #
+    # @example
+    #   layout(carousel) {
+    #     layout(UIImage, backgroundColor: UIColor.colorWithImagePattern(image)
+    #   }
+    #
+    # Or to modify the properties of an element:
+    # 
+    # @example
+    #   layout(carousel, width: 500, height: 100)
+    #
     def layout(class_or_instance, name_or_properties, properties_or_nil=nil, &block)
       instance = Class === class_or_instance ? class_or_instance.new : class_or_instance
 
@@ -41,9 +108,9 @@ module Teacup
         properties = nil
       end
 
-      instance.style_sheet = style_sheet
+      instance.stylesheet = stylesheet
       instance.style(properties) if properties
-      instance.style_name = name if name
+      instance.stylename = name if name
 
       if !instance.superview && !(instance == top_level_view)
         (superview_chain.last || top_level_view).addSubview(instance)
@@ -58,6 +125,50 @@ module Teacup
       instance
     end
 
+    # Returns a stylesheet to use to style the contents of this controller's
+    # view.
+    #
+    # This method will be queried each time {restyle!} is called, and also
+    # implicitly # whenever Teacup needs to draw your layout (currently only at
+    # view load time).
+    #
+    # @return Teacup::Stylesheet
+    #
+    # @example
+    #
+    #  def stylesheet
+    #    if [UIDeviceOrientationLandscapeLeft,
+    #        UIDeviceOrientationLandscapeRight].include?(UIDevice.currentDevice.orientation)
+    #      Teacup::Stylesheet::IPad
+    #    else
+    #      Teacup::Stylesheet::IPadVertical
+    #    end
+    #  end
+    def stylesheet
+      nil
+    end
+
+    # Instruct teacup to reapply styles to your subviews
+    #
+    # You should call this whenever the return value of your stylesheet meethod
+    # would change,
+    #
+    # @example
+    #   def willRotateToInterfaceOrientation(io, duration: duration)
+    #     restyle!
+    #   end
+    def restyle!
+      top_level_view.stylesheet = stylesheet
+    end
+
+    protected
+
+    # Get's the top-level UIView for this object.
+    #
+    # This can either be 'self' if the current object is in fact a UIView,
+    # or 'view' if it's a controller.
+    #
+    # @return UIView
     def top_level_view
       case self
       when UIViewController
@@ -67,6 +178,10 @@ module Teacup
       end
     end
 
+    # Get's the current stack of views in nested calls to layout.
+    #
+    # The view at the end of the stack is the one into which subviews
+    # are currently being attached.
     def superview_chain
       @superview_chain ||= []
     end
--- lib/view.rb
+++ lib/view.rb
@@ -1,28 +1,74 @@
 module Teacup
+  # Teacup::View defines some utility functions for UIView that enable
+  # a lot of the magic for Teacup::Layout.
+  #
+  # Most users of teacup should be able to ignore the contents of this file
+  # for the most part.
   module View
-    attr_reader :style_name, :style_sheet
+    # The current stylename that is used to look up properties in the stylesheet.
+    attr_reader :stylename
+    
+    # The current stylesheet will be looked at when properties are needed.
+    attr_reader :stylesheet
 
-    def style_name=(style_name)
-      @style_name = style_name
-      style(style_sheet.query(style_name)) if style_sheet
+    # Alter the stylename of this view.
+    #
+    # This will cause new styles to be applied from the stylesheet.
+    #
+    # @param Symbol  stylename
+    def stylename=(stylename)
+      @stylename = stylename
+      style(stylesheet.query(stylename)) if stylesheet
     end
 
-    def style_sheet=(style_sheet)
-      @style_sheet = style_sheet
-      style(style_sheet.query(style_name)) if style_name && style_sheet
-      subviews.each{ |subview| subview.style_sheet = style_sheet }
+    # Alter the stylesheet of this view.
+    #
+    # This will cause new styles to be applied using the current stylename,
+    # and will recurse into subviews.
+    #
+    # If you would prefer that a given UIView object does not inherit the
+    # stylesheet from its parents, override the 'stylesheet' method to
+    # return the correct value at all times.
+    #
+    # @param Teacup::Stylesheet  stylesheet.
+    def stylesheet=(stylesheet)
+      @stylesheet = stylesheet
+      style(stylesheet.query(stylename)) if stylename && stylesheet
+      subviews.each{ |subview| subview.stylesheet = stylesheet }
     end
 
-    def animate_to_style_name(style_name, options={})
-      return if self.style_name == style_name
+    # Animate a change to a new stylename.
+    #
+    # This is equivalent to wrapping a call to .stylename= inside
+    # UIView.beginAnimations.
+    #
+    # @param Symbol  the new stylename
+    # @param Options  the options for the animation (may include the
+    #                 duration and the curve)
+    #
+    def animate_to_stylename(stylename, options={})
+      return if self.stylename == stylename
       UIView.beginAnimations(nil, context: nil)
       # TODO: This should be in a style-sheet!
       UIView.setAnimationDuration(options[:duration]) if options[:duration]
       UIView.setAnimationCurve(options[:curve]) if options[:curve]
-      self.style_name = style_name
+      self.stylename = stylename
       UIView.commitAnimations
     end
 
+    # Apply style properties to this element.
+    #
+    # Takes a hash of properties such as may have been read from a stylesheet
+    # or passed as parameters to {Teacup::Layout#layout}, and applies them to
+    # the element.
+    #
+    # Does a little bit of magic (that may be split out as 'sugarcube') to
+    # make properties work as you'd expect.
+    #
+    # If you try and assign something in properties that is not supported,
+    # a warning message will be emitted.
+    #
+    # @param Hash  the properties to set.
     def style(properties)
       clean_properties! properties
 
@@ -50,6 +96,16 @@ module Teacup
       end
     end
 
+    # merge definitions for 'frame' into one.
+    #
+    # To support 'extends' more nicely it's convenient to split left, top, width
+    # and height out of frame. Unfortunately that means we have to write ugly
+    # code like this to reassemble them into what the user actually meant.
+    #
+    # WARNING: this method *mutates* its parameter.
+    #
+    # @param Hash
+    # @return Hash
     def clean_properties!(properties)
       return unless [:frame, :left, :top, :width, :height].any?(&properties.method(:key?))
 
@@ -61,6 +117,7 @@ module Teacup
       frame[1][1] = properties.delete(:height) || frame[1][1]
 
       properties[:frame] = frame
+      properties
     end
   end
 end
