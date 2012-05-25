85077638c818dd75ddc8c606a0438742a4673e74
Conrad Irwin <conrad.irwin@gmail.com>
2012-05-25 00:18:36 -0700

    Add specs for Teacup::View

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,4 @@ require 'rspec'
 require File.expand_path '../../lib/teacup/version.rb', __FILE__
 require File.expand_path '../../lib/teacup/contributors.rb', __FILE__
 require File.expand_path '../../lib/teacup/stylesheet.rb', __FILE__
+require File.expand_path '../../lib/teacup/view.rb', __FILE__
