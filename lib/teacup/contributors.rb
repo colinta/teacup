10ce019f5706884852977c190bdb0033a2cbc0fa
Colin Thomas-Arnold <colinta@gmail.com>
2012-06-01 09:48:03 -0600

    meh, don't need grammar skills

--- lib/teacup/contributors.rb
+++ lib/teacup/contributors.rb
@@ -1,7 +1,6 @@
 module Teacup
 
   # If you've made a contribution, add your name here in :' ... ' as a constant symbol.
-  # Please place names in aphabetical order.
   CONTRIBUTORS = ['Chris Clarke', 'Colin Thomas-Arnold', 'Conrad Irwin', 'Roland Oth', 'Vladimir Pouzanov']
 
 end
