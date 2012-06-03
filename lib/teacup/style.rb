b4430432abf4d91f85d0d9f58264f15cd1f2282c
Colin Thomas-Arnold <colinta@gmail.com>
2012-06-03 14:14:09 -0600

    nested blocks

--- lib/teacup/style.rb
+++ lib/teacup/style.rb
@@ -1,13 +1,19 @@
 
 module Teacup
+
   class Style < Hash
     def initialize(*args, &block)
       super
       instance_eval &block
     end
 
-    def method_missing(name, *args)
-      self[name] = args.shift
+    def method_missing(name, *args, &block)
+      if block
+        self[name] = Teacup::Style.new(args, &block)
+      else
+        self[name] = args.shift
+      end
     end
   end
+
 end
