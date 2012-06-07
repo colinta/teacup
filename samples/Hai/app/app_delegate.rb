232ec3784d487862722c8813df6e3c6e60fd1f07
Colin Thomas-Arnold <colinta@gmail.com>
2012-06-06 21:02:58 -0600

    don't need full screen layout

--- samples/Hai/app/app_delegate.rb
+++ samples/Hai/app/app_delegate.rb
@@ -1,9 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = HaiViewController.alloc.init.tap{ |c| c.wantsFullScreenLayout = true }
+    @window.rootViewController = HaiViewController.alloc.init
     @window.makeKeyAndVisible
-  
+
     true
   end
 end
