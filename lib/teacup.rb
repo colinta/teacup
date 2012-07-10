3d8339d2221c9bc81fa648ffbff3493ada67bb88
Colin Thomas-Arnold <colinta@gmail.com>
2012-07-09 23:43:45 -0600

    include dummy.rb - fix

--- lib/teacup.rb
+++ lib/teacup.rb
@@ -7,5 +7,5 @@ Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'teacup/**/*.rb')).each do |file|
     app.files.unshift(file)
   end
-  app.files.unshift('dummy.rb')
+  app.files.unshift(File.join(File.dirname(__FILE__), 'dummy.rb'))
 end
