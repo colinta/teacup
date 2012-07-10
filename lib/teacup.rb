b66e3f536ef4b91fdd02c37f1de2159e415bd3a6
Colin Thomas-Arnold <colinta@gmail.com>
2012-07-09 23:42:33 -0600

    include dummy.rb

--- lib/teacup.rb
+++ lib/teacup.rb
@@ -7,4 +7,5 @@ Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'teacup/**/*.rb')).each do |file|
     app.files.unshift(file)
   end
+  app.files.unshift('dummy.rb')
 end
--- lib/teacup/version.rb
+++ lib/teacup/version.rb
@@ -1,5 +1,5 @@
 module Teacup
 
-  VERSION = '0.2.1'
+  VERSION = '0.2.2'
 
 end
