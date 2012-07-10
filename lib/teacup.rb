ff6e4078140686e7577f07db2adeb2ab2d48dd1f
Colin Thomas-Arnold <colinta@gmail.com>
2012-07-09 23:35:16 -0600

    gem file

---
+++ lib/teacup.rb
@@ -0,0 +1,10 @@
+unless defined?(Motion::Project::Config)
+  raise "This file (teacup) must be required within a RubyMotion project Rakefile."
+end
+
+
+Motion::Project::App.setup do |app|
+  Dir.glob(File.join(File.dirname(__FILE__), 'teacup/**/*.rb')).each do |file|
+    app.files.unshift(file)
+  end
+end
