fe6b1e534130ba5d82f5c71e31cb8c6b0f3db917
Colin Thomas-Arnold <colinta@gmail.com>
2012-07-13 18:13:17 -0600

    require fixes - requires 'in order' (more or less...)

--- lib/teacup.rb
+++ lib/teacup.rb
@@ -4,8 +4,21 @@ end
 
 
 Motion::Project::App.setup do |app|
+  # scans app.files until it finds app/ (the default)
+  # if found, it inserts just before those files, otherwise it will insert to
+  # the end of the list
+  insert_point = 0
+  app.files.each_index do |index|
+    file = app.files[index]
+    if file =~ /^(?:\.\/)?app\//
+      # found app/, so stop looking
+      break
+    end
+    insert_point = index + 1
+  end
+
   Dir.glob(File.join(File.dirname(__FILE__), 'teacup/**/*.rb')).reverse.each do |file|
-    app.files.unshift(file)
+    app.files.insert(insert_point, file)
   end
   app.files.push(File.join(File.dirname(__FILE__), 'dummy.rb'))
 end
