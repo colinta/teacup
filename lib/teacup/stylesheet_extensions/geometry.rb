f4ce843d66f87f9b722102d18c5daee5135a1d29
Colin Thomas-Arnold <colinta@gmail.com>
2012-09-24 21:48:45 -0600

    geometry extensions

---
+++ lib/teacup/stylesheet_extensions/geometry.rb
@@ -0,0 +1,39 @@
+module Teacup
+  class Stylesheet
+    def iPhone       ; 1 << 1 ; end
+    def iPhoneRetina ; 1 << 2 ; end
+    def iPhone5      ; 1 << 3 ; end
+    def iPad         ; 1 << 4 ; end
+    def iPadRetina   ; 1 << 5 ; end
+
+
+    def screen_size
+      UIScreen.mainScreen.bounds.size
+    end
+
+    def app_size
+      UIScreen.mainScreen.applicationFrame.size
+    end
+
+    def device
+      this_device = 0;
+      if UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPhone
+        this_device |= iPhone
+        if UIScreen.mainScreen.respondsToSelector(:scale)
+          this_device |= iPhoneRetina
+          if UIScreen.mainScreen.bounds.size.height == 568
+            this_device |= iPhone5
+          end
+        end
+      else
+        this_device |= iPad
+        if UIScreen.mainScreen.respondsToSelector(:scale)
+          this_device |= iPadRetina
+        end
+      end
+
+      return this_device
+    end
+
+  end
+end
--- lib/teacup/version.rb
+++ lib/teacup/version.rb
@@ -1,5 +1,5 @@
 module Teacup
 
-  VERSION = '0.3.12'
+  VERSION = '0.3.13'
 
 end
