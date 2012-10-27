b998392767ed47ea2347f9ec59fa9178b1decf55
Colin Thomas-Arnold <colinta@gmail.com>
2012-10-27 17:22:25 -0600

    dont_restyle fixes

--- lib/teacup/restyle.rb
+++ lib/teacup/restyle.rb
@@ -1,22 +1,22 @@
 module Teacup
   module_function
 
-  def dont_restyle
+  def dont_restyle?
     @dont_restyle ||= nil
   end
 
   def should_restyle?
-    return ! self.dont_restyle
+    return ! self.dont_restyle?
   end
 
   def should_restyle_and_block
     should_restyle = self.should_restyle?
-    @dont_style = true
+    @dont_restyle = true
     return should_restyle
   end
 
   def should_restyle!
-    @dont_style = nil
+    @dont_restyle = nil
   end
 
 end
