dbb2d77b91d52d7449f07baef689e8c82fb00fd7
Colin Thomas-Arnold <colinta@gmail.com>
2012-10-31 13:34:59 -0600

    just get rid of the status bar altogether - the test STILL fails!

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,7 +3,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     return true if RUBYMOTION_ENV == 'test'
 
-    application.setStatusBarStyle(UIStatusBarStyleBlackTranslucent)
+    application.setStatusBarHidden(true, withAnimation:UIStatusBarAnimationSlide)
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = FirstController.new
