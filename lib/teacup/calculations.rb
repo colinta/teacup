1de4882b80b1f6b6eee085d587ea95b06aa11d0b
Colin T.A. Gray <colinta@gmail.com>
2013-04-07 15:07:48 -0600

    new calculations!  you can add and substract offsets (css needs this trick)

--- lib/teacup/calculations.rb
+++ lib/teacup/calculations.rb
@@ -1,19 +1,23 @@
 module Teacup
   module_function
-  def calculate(view, dimension, percent)
-    if percent.is_a? Proc
-      view.instance_exec(&percent)
-    elsif percent.is_a? String and percent[-1] == '%'
-      percent = percent[0...-1].to_f / 100.0
+  def calculate(view, dimension, amount)
+    if amount.is_a? Proc
+      view.instance_exec(&amount)
+    elsif amount.is_a?(String) && amount.include?('%')
+      location = amount.index '%'
+      offset = amount[(location+1)..-1].gsub(' ', '').to_f
+      percent = amount[0...location].to_f / 100.0
 
       case dimension
       when :width
-        CGRectGetWidth(view.superview.bounds) * percent
+        CGRectGetWidth(view.superview.bounds) * percent + offset
       when :height
-        CGRectGetHeight(view.superview.bounds) * percent
+        CGRectGetHeight(view.superview.bounds) * percent + offset
+      else
+        raise "Unknown dimension #{dimension}"
       end
     else
-      percent
+      amount
     end
   end
 end
---
+++ spec/calculations_spec.rb
@@ -0,0 +1,55 @@
+class Viewish
+  def superview
+    @superview ||= Viewish.new
+  end
+
+  def bounds
+    CGRect.new([0, 0,], [100, 44])
+  end
+end
+
+
+describe 'Teacup.calculate' do
+
+  it 'should return static numbers' do
+    Teacup.calculate(nil, nil, 1).should == 1
+  end
+
+  it 'should call blocks' do
+    a = 'hi!'
+    Teacup.calculate(a, nil, ->{
+      self.should == a
+      2
+    }).should == 2
+  end
+
+  it 'should return percents with :width' do
+    Teacup.calculate(Viewish.new, :width, '50%').should == 50
+  end
+
+  it 'should return percents with :height' do
+    Teacup.calculate(Viewish.new, :height, '50%').should == 22
+  end
+
+  describe 'should return percents with offset' do
+    it ':width, 50% + 10' do
+      Teacup.calculate(Viewish.new, :width, '50% + 10').should == 60
+    end
+    it ':width, 50% - 10' do
+      Teacup.calculate(Viewish.new, :width, '50% - 10').should == 40
+    end
+    it ':width, 25% + 5' do
+      Teacup.calculate(Viewish.new, :width, '25% + 5').should == 30
+    end
+    it ':height, 50% + 10' do
+      Teacup.calculate(Viewish.new, :height, '50% + 10').should == 32
+    end
+    it ':height, 50% - 10' do
+      Teacup.calculate(Viewish.new, :height, '50% - 10').should == 12
+    end
+    it ':height, 25% + 5' do
+      Teacup.calculate(Viewish.new, :height, '25% + 5').should == 16
+    end
+  end
+
+end
