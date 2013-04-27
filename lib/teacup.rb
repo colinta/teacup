4e95f03404ab183cb88e9ceaceacf405f605e4a0
Colin T.A. Gray <colinta@gmail.com>
2013-04-27 15:23:08 -0600

    updated insert_point code

--- lib/teacup.rb
+++ lib/teacup.rb
@@ -7,15 +7,7 @@ Motion::Project::App.setup do |app|
   # scans app.files until it finds app/ (the default)
   # if found, it inserts just before those files, otherwise it will insert to
   # the end of the list
-  insert_point = 0
-  app.files.each_index do |index|
-    file = app.files[index]
-    if file =~ /^(?:\.\/)?app\//
-      # found app/, so stop looking
-      break
-    end
-    insert_point = index + 1
-  end
+  insert_point = app.files.find_index { |file| file =~ /^(?:\.\/)?app\// } || 0
 
   app.files.insert(insert_point, File.join(File.dirname(__FILE__), 'dummy.rb'))
   Dir.glob(File.join(File.dirname(__FILE__), 'teacup/**/*.rb')).reverse.each do |file|
