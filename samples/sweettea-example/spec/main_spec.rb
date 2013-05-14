4c1b63ff20f13f0385991a3feb6cc19b31f92dfa
Colin T.A. Gray <colinta@gmail.com>
2013-05-14 10:00:08 -0600

    rm useless specs

--- samples/sweettea-example/spec/main_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Application 'teacup-example'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  it "has one window" do
-    @app.windows.size.should == 1
-  end
-end
--- samples/teacup-osx/spec/main_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Application 'teacup-osx'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  it "has one window" do
-    @app.windows.size.should == 1
-  end
-end
