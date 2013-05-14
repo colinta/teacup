76d79734880ec078bc1037a90ceda7d3b1230bae
Colin T.A. Gray <colinta@gmail.com>
2013-05-14 09:25:56 -0600

    fixes to some samples

--- samples/Hai/app/styles/iphone.rb
+++ samples/Hai/app/styles/iphone.rb
@@ -11,30 +11,37 @@ Teacup::Stylesheet.new(:iphone) do
   style :label,
     text: 'Hai!',
     backgroundColor: UIColor.whiteColor,
-    top: 10,
-    left: 100,
-    width: 50,
-    height: 20,
-    layer: {
-      transform: identity,
+    portrait: {
+      layer: {
+        transform: transform_layer.identity,
+      },
+      top: 10,
+      left: 100,
+      height: 20,
+      width: 50
     },
     landscape_left: {
       layer: {
-        transform: rotate(identity, pi/6, 0.3, 0.3, 0.3)
+        transform: transform_layer.rotate(pi/6, 0.3, 0.3, 0.3)
       },
+      top: 40,
+      left: 100,
+      height: 20,
       width: 75
     }
 
   style :footer,
     text: 'brought to you by teacup',
     backgroundColor: UIColor.lightGrayColor,
-    top: 440,
-    left: 120,
     width: 200,
     height: 20,
+    portrait: {
+      top: 440,
+      left: 120,
+    },
     landscape: {
+      top: 280,
       left: 280,
-      top: 280
     }
 
 end
