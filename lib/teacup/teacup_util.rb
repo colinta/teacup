b6182663e389f82aa3042e47e2afeef763b3e5ba
Gabriel Reis <gabriel.oreis+greis@gmail.com>
2013-07-28 11:51:39 -0400

    Only use first level subviews for motion-layout
    * Motion Layout adds all passed views as subviews of the current view,
    * so only the first level subviews should be passed as a parameter.
    * This fixes an issue where a subview of a subview was moving to the
    * parent view.

--- lib/teacup/teacup_util.rb
+++ lib/teacup/teacup_util.rb
@@ -4,11 +4,7 @@ module Teacup
   # Returns all the subviews of `target` that have a stylename.  `target` is not
   # included in the list.  Used by the motion-layout integration in layout.rb.
   def get_styled_subviews(target)
-    retval = target.subviews.select { |v| v.stylename }
-    retval.concat(target.subviews.map do |subview|
-      get_styled_subviews(subview)
-    end)
-    retval.flatten
+    target.subviews.select { |v| v.stylename }
   end
 
   def to_instance(class_or_instance)
