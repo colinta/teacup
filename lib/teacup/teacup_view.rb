9134f78f3f2db65e3b519ea4bfabe8467f363b14
Colin T.A. Gray <colinta@gmail.com>
2013-11-06 08:56:56 -0700

    added a note about these messages

--- lib/teacup/teacup_view.rb
+++ lib/teacup/teacup_view.rb
@@ -187,6 +187,7 @@ module Teacup
 
         if original_constraint.relative_to && ! constraint.relative_to
           container = self
+          puts "Searching for #{original_constraint.relative_to.inspect} in the tree:"
           tab = ''
           while container
             tab << '->'
